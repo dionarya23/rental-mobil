3af75e842c3759113c68ce0059f90747253611bb
Dion Arya Pamungkas <dionarya.p@gmail.com>
2020-01-22 15:22:03 +0700

    finish ireport

--- src/datamobil/FSewa.java
+++ src/datamobil/FSewa.java
@@ -16,6 +16,12 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.view.JasperViewer;
 
 /**
  *
@@ -291,8 +297,21 @@ public class FSewa extends javax.swing.JFrame {
     }//GEN-LAST:event_BtnLaporanActionPerformed
     
     private void buat_laporan() {
-        System.out.println("Buat Laporan!!!");
-    }
+        String reportSource;
+        String reportDest;
+            try{
+                       reportSource=System.getProperty("user.dir")+"/report1.jrxml";
+                       reportDest=System.getProperty("user.dir")+"/report1.html";
+
+                       JasperReport jasperReport=JasperCompileManager.compileReport(reportSource);
+                       JasperPrint jasperPrint=JasperFillManager.fillReport(jasperReport,null,conn);
+                       JasperExportManager.exportReportToHtmlFile(jasperPrint,reportDest);
+                       JasperViewer.viewReport(jasperPrint,false);
+
+            }catch(Exception e){
+                   System.out.println(e);
+            }
+    }                           
     
     private void tambah() {
          try{            
